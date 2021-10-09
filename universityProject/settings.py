26e0b4c938137aff33320ffc0bcd9e28c57746a1
Santiago Meneses <santiagome2014@hotmail.com>
2021-10-09 12:10:36 -0500

    Deployment in heroku

--- universityProject/settings.py
+++ universityProject/settings.py
@@ -150,3 +150,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Configuracion de django para heroku.
+import django_heroku
+django_heroku.settings(locals())
