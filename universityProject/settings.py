7fe4286394228563c7650e8a3427e9f390e7ebc8
Santiago Meneses <santiagome2014@hotmail.com>
2021-10-30 17:00:05 -0500

    cors allow front site

--- universityProject/settings.py
+++ universityProject/settings.py
@@ -88,10 +88,8 @@ MIDDLEWARE = [
 # Origenes.
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
     "http://localhost:8080",
-    "http://127.0.0.1:9000",
+    "https://university-enrollments-front.herokuapp.com/login"
 ]
 
 ROOT_URLCONF = 'universityProject.urls'
