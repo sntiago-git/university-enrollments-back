5aee64e13aaf94ffbb516c6ebdf7bdee5c519f3c
Santiago Meneses <santiagome2014@hotmail.com>
2021-11-01 19:04:30 -0500

    added str to career model

--- universityApp/models/career.py
+++ universityApp/models/career.py
@@ -3,3 +3,7 @@ from django.db import models
 class Career(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
     name = models.TextField("Name", max_length=20, unique=True)
+
+    def __str__(self):
+        
+        return self.name
